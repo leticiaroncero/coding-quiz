256f094a5e30e8763836525b9f08f462d45e80f0
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-08-24 23:29:27 -0700

    expand game logic

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -27,31 +27,49 @@ var endGame = function () {
 }
 
 var startGame = function () {
+    startTimer();
+    nextQuestion();
+}
+
+function nextQuestion() {
     var initialScreen = document.querySelector("#start-screen");
+    initialScreen.setAttribute("class", "hide");
 
     var currentQuestion = questions[currentQuestionIndex];
 
     var questionTitle = document.querySelector("#question-title");
     questionTitle.textContent = currentQuestion.title;
 
-    var optionsList = document.createElement("ul");
-    questionOptions.appendChild(optionsList);
+    questionOptions.textContent = "";
 
     for (var i = 0; i < currentQuestion.choices.length; i++) {
-        var listItem = document.createElement("li")
+        var listItem = document.createElement("button")
         var optionsListItem = currentQuestion.choices[i];
-        listItem.textContent = optionsListItem;
-        optionsList.appendChild(listItem);
+        listItem.textContent = (i + 1) + ". " + optionsListItem;
+        listItem.setAttribute("data-answer", optionsListItem);
+        questionOptions.appendChild(listItem);
+
+        listItem.addEventListener("click", checkAnswer);
     }
-
-    
-
-    questionOptions.textContent = currentQuestion.choices
-
-    initialScreen.setAttribute("class", "hide");
-    startTimer()
 }
 //event listener
+
+var checkAnswer = function (event) {
+    var answerEl = document.querySelector("#feedback")
+
+    if (event.target.getAttribute("data-answer") === questions[currentQuestionIndex].answer) {
+        answerEl.textContent = "Correct!"
+    } else {
+        answerEl.textContent = "Incorrect!"
+    }
+
+    currentQuestionIndex++;
+    setTimeout(function () {
+         answerEl.setAttribute("class", "feedback hide") 
+        }, 1000)
+
+    nextQuestion();
+}
 
 var startButton = document.querySelector("#start");
 
